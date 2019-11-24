d5cee257f14eeef9c0c4b78ad378d1aee81b110c
Colby Cox <uoflcards22@gmail.com>
2019-11-23 22:54:53 -0500

    add fake user agent so megabus will allow us info

--- megabus.py
+++ megabus.py
@@ -10,10 +10,15 @@ DEST_CITIES_URL = lambda origin: "https://us.megabus.com/journey-planner/api/des
 DATES_URL = lambda origin, destination: "https://us.megabus.com/journey-planner/api/journeys/travel-dates?originCityId={}&destinationCityId={}".format(origin, destination)
 PRICES_URL = lambda origin, destination, departure, passengers: "https://us.megabus.com/journey-planner/api/journeys?originId={}&destinationId={}&departureDate={}&totalPassengers={}".format(origin, destination, departure, passengers)
 
+USER_AGENT_STR = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
+
+HEADERS = {
+    'User-Agent': USER_AGENT_STR        
+}
 
 def get_origin_cities():
     """Get origin cities."""
-    r = requests.get(ORIGIN_CITIES_URL)
+    r = requests.get(ORIGIN_CITIES_URL, headers=HEADERS)
     try:
         return r.json()["cities"]
     except Exception as e:
@@ -21,7 +26,7 @@ def get_origin_cities():
 
 
 def get_destination_cities(origin_city_id):
-    r = requests.get(DEST_CITIES_URL(origin_city_id))
+    r = requests.get(DEST_CITIES_URL(origin_city_id), headers=HEADERS)
     try:
         return r.json()["cities"]
     except Exception as e:
@@ -29,7 +34,7 @@ def get_destination_cities(origin_city_id):
 
 
 def get_dates(origin_city_id, destination_city_id):
-    r = requests.get(DATES_URL(origin_city_id, destination_city_id))
+    r = requests.get(DATES_URL(origin_city_id, destination_city_id), headers=HEADERS)
     try:
         return r.json()["availableDates"]
     except Exception as e:
@@ -38,7 +43,7 @@ def get_dates(origin_city_id, destination_city_id):
 
 def get_prices(origin_city_id, destination_city_id, departure, passengers):
     r = requests.get(PRICES_URL(origin_city_id, destination_city_id, departure,
-                                passengers))
+                                passengers), headers=HEADERS)
     try:
         return r.json()["journeys"]
     except Exception as e:
